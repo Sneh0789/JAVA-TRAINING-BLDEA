3b5a60b0766e30f5625c78d0b2dd0ba745ce4cda
sneha <sneha@HP>
2023-03-20 19:30:00 +0530

    Third commit

---
+++ src/Text1/Text2.java
@@ -0,0 +1,20 @@
+package Text1;
+
+public class Text2 {
+
+	public static void main(String[] args) {
+				for(int i=1;i<=5;i++)
+				{
+					System.out.print("* ");
+				}
+				// TODO Auto-generated method stub
+
+			}
+
+		
+
+		// TODO Auto-generated method stub
+
+	}
+
+
